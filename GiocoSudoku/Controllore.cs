ac20eac398b395d6cbc00389e794b5dbb180b388
CremonaGiacomo <CremonaGiacomo@CLOUDJ-CREMONA>
2026-01-29 17:53:53 +0100

    Creazione della classe controllore

---
+++ GiocoSudoku/Controllore.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GiocoSudoku
+{
+    internal class Controllore
+    {
+        // Implementazione futura del controllore del gioco Sudoku
+
+        public Controllore() {}
+
+        public int[][] matrice;
+        public void InizializzaMatrice()
+        {
+            matrice = new int[9][];
+            for (int i = 0; i < 9; i++)
+            {
+                matrice[i] = new int[9];
+            }
+        }
+
+        public void ControllaSudoku()
+        {
+            //controllo per righe
+            for (int i = 0; i < 9; i++)
+            {
+                matrice[i][0] = 0;
+            }
+
+            for (int i = 0;i < 9; i++)
+            {
+                for(int j = 0; j < 9; j++)
+                    Console.Write(matrice[i][j] + " ");
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
--- GiocoSudoku/Program.cs
+++ GiocoSudoku/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            
+            Controllore controllore = new Controllore();
+            controllore.InizializzaMatrice();
+            controllore.ControllaSudoku();
         }
     }
 }
